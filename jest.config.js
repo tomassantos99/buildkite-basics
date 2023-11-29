4481acfdd03f4bafdacd3f25850f73ac6fddce2a
santost-fd <tomas.santos@fanduel.com>
2023-11-29 11:42:01 +0000

    Added coverage. Should fail because of subtract.

--- jest.config.js
+++ jest.config.js
@@ -180,7 +180,7 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
     '<rootDir>/node_modules/(?!axios)/'
-  ]
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
@@ -193,4 +193,8 @@ module.exports = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!**/node_modules/**'
+  ]
 };
